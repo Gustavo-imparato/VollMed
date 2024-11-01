5a108d8520ab237644700bbeceff3f0f52569919
Gustavo-imparato <129402642+Gustavo-imparato@users.noreply.github.com>
2024-10-31 21:28:54 -0300

    Delete api directory

--- api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.paciente;
-
-public record DadosListagemPaciente(Long id, String nome, String cpf, String email, String telefone) {
-    public DadosListagemPaciente(Paciente paciente){
-        this(paciente.getId(), paciente.getNome(), paciente.getCpf(), paciente.getEmail(), paciente.getTelefone());
-    }
-}
